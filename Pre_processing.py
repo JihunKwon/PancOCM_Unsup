e44e5f099de820707efd211578148dab9fef599e
JihunKwon <jihun.k@frontier.hokudai.ac.jp>
2019-11-16 15:04:20 +0900

    filtering added and visualization worked

--- Pre_processing.py
+++ Pre_processing.py
@@ -3,19 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 import time
+import statistics
 
 # Load the data
 out_list = []
 out_list.append("C:\\Users\\Kwon\\Documents\\Panc_OCM\\Subject_02_20181220\\run1.npy")
 out_list.append("C:\\Users\\Kwon\\Documents\\Panc_OCM\\Subject_02_20181220\\run2.npy")
 
-sr_list = ['s1r1', 's1r1', 's1r2', 's1r2', 's2r1', 's2r1', 's2r2', 's2r2', 's3r1', 's3r1', 's3r2', 's3r2']
+sr_list = ['s2r2', 's2r2']
 rep_list = [3690, 3690]
 
 num_train = 3
 num_test = 10 - num_train
 num_ocm = 3
 num_bh = 5 # number of bh in each state
+s_new = 296  # the depth your interest
 
 def outlier_remove(Sub_run_name, c0, ocm0):  # input subject and run name
     count = 0
@@ -69,6 +71,11 @@ def preprocessing(fidx):
     ocm = ocm[300:650, :]
     s, t = np.shape(ocm) # s=# of samples per trace. t=# of total traces
 
+    # variables initialization
+    median0 = np.zeros([s_new, num_bh])  # median
+    median1 = np.zeros([s_new, num_bh])
+    median2 = np.zeros([s_new, num_bh])
+
     # divide the data into each OCM and store absolute value
     b = np.linspace(0, t - 1, t)
     b0 = np.mod(b, 4) == 0
@@ -91,9 +98,70 @@ def preprocessing(fidx):
     ocm0_new = outlier_remove(Sub_run_name, c0_new, ocm0)
     s, c0_new_removed = np.shape(ocm0_new)
     t_sub_removed = int(c0_new_removed / num_bh)
+    dif = ocm0.shape[1]-ocm0_new.shape[1]
 
-    '''
-    Skip filtering part for now
-    '''
 
-    return ocm0_new, ocm1, ocm2
+    #return ocm0_new, ocm1, ocm2
+
+
+    ## Filtering starts here
+    ocm0_filt = np.zeros([s_new, c0_new_removed])  # filtered signal (median based filtering)
+    ocm1_filt = np.zeros([s_new, c0_new])
+    ocm2_filt = np.zeros([s_new, c0_new])
+    ocm0_low = np.zeros([s_new, c0_new_removed])  # low pass
+    ocm1_low = np.zeros([s_new, c0_new])
+    ocm2_low = np.zeros([s_new, c0_new])
+    median0_low = np.zeros([s_new, num_bh])
+    median1_low = np.zeros([s_new, num_bh])
+    median2_low = np.zeros([s_new, num_bh])
+    f1 = np.ones([10])  # low pass kernel
+
+    # Median-based filtering
+    for bh in range(0, num_bh):
+        for depth in range(0, s_new):
+            # get median for each bh
+            median0[depth, bh] = statistics.median(ocm0_new[depth, bh * t_sub_removed:(bh + 1) * t_sub_removed])
+            median1[depth, bh] = statistics.median(ocm1[depth, bh * t_sub:(bh + 1) * t_sub])
+            median2[depth, bh] = statistics.median(ocm2[depth, bh * t_sub:(bh + 1) * t_sub])
+
+    # Filter the median
+    length = 50  # size of low pass filter
+    f1_medi = np.ones([length])
+    for bh in range(0, num_bh):
+        tr0_medi = median0[:, bh]
+        tr1_medi = median1[:, bh]
+        tr2_medi = median2[:, bh]
+        median0_low[:, bh] = np.convolve(tr0_medi, f1_medi, 'same') / length
+        median1_low[:, bh] = np.convolve(tr1_medi, f1_medi, 'same') / length
+        median2_low[:, bh] = np.convolve(tr2_medi, f1_medi, 'same') / length
+
+    # filtering all traces with median trace
+    # The size of ocm0 is different with ocm1 and ocm2. Has to be filtered separately.
+    ## OCM0
+    bh = -1
+    for p in range(0, c0_new_removed):
+        # have to consider the number of traces removed
+        if p % rep_list[fidx] - dif // num_bh == 0:
+            bh = bh + 1
+        for depth in range(0, s_new):
+            # filter the signal (subtract median from each trace of corresponding bh)
+            ocm0_filt[depth, p] = ocm0_new[depth, p] - median0_low[depth, bh]
+        tr0 = ocm0_filt[:, p]
+        ocm0_low[:, p] = np.convolve(np.sqrt(np.square(tr0)), f1, 'same')
+
+    ## OCM1 and 2
+    bh = -1
+    for p in range(0, c0_new):
+        if p % rep_list[fidx] == 0:
+            bh = bh + 1
+        for depth in range(0, s_new):
+            # filter the signal (subtract median from each trace of corresponding bh)
+            ocm1_filt[depth, p] = ocm1[depth, p] - median1_low[depth, bh]
+            ocm2_filt[depth, p] = ocm2[depth, p] - median2_low[depth, bh]
+        tr1 = ocm1_filt[:, p]
+        tr2 = ocm2_filt[:, p]
+        ocm1_low[:, p] = np.convolve(np.sqrt(np.square(tr1)), f1, 'same')
+        ocm2_low[:, p] = np.convolve(np.sqrt(np.square(tr2)), f1, 'same')
+
+    return ocm0_low, ocm1_low, ocm2_low
+
--- VAE_OCM_anomaly_detection.py
+++ VAE_OCM_anomaly_detection.py
@@ -6,7 +6,6 @@
 # This kernel trains a Variational Autoencoder in Keras with Gaussian input and output.
 
 # import Keras and other libralies:
-
 import os
 import numpy as np
 import pandas as pd
@@ -22,167 +21,192 @@ from keras.layers import Input, Dense, Lambda, BatchNormalization
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from keras.utils import plot_model
+#from keras import metrics
 
 num_subject = 1
 num_train = 3
-num_test = 10 - num_train
+num_test = 1
 num_ocm = 3
 
+ocm_channel = 1 # 'all', 0, 1, and 2. All is combination of three OCMs
+
 # Read the dataset
-for fidx in range(0, num_subject*2):
-    ocm0, ocm1, ocm2 = Pre_processing.preprocessing(fidx)
+for fidx in range(0, num_subject*2, 2):
+    print('state1 importing...')
+    ocm0_pre, ocm1_pre, ocm2_pre = Pre_processing.preprocessing(fidx)
+    print('state2 importing...')
+    ocm0_post, ocm1_post, ocm2_post = Pre_processing.preprocessing(fidx+1)
 
     # set all OCM shape same
-    ocm1 = ocm1[:, :np.size(ocm0[1])]
-    ocm2 = ocm2[:, :np.size(ocm0[1])]
-    print('ocm0 shape', ocm0.shape) # (350, 16934)
+    ocm1_pre = ocm1_pre[:, :np.size(ocm0_pre[1])]
+    ocm2_pre = ocm2_pre[:, :np.size(ocm0_pre[1])]
+    print('ocm0 shape pre', ocm0_pre.shape) # (350, 16934)
+
+    ocm1_post = ocm1_post[:, :np.size(ocm0_post[1])]
+    ocm2_post = ocm2_post[:, :np.size(ocm0_post[1])]
+    print('ocm0 shape post', ocm0_post.shape) # (350, 16934)
+
+    ## Use only bh=5 for state 2
+    ocm0_post = ocm0_post[:, :num_test*int(np.size(ocm0_post[1])/5)]
+    ocm1_post = ocm1_post[:, :num_test*int(np.size(ocm0_post[1])/5)]
+    ocm2_post = ocm2_post[:, :num_test*int(np.size(ocm0_post[1])/5)]
+    print('ocm0 shape post for bh5', ocm0_post.shape)
 
     # Split to train and test bh
-    ocm0_train = ocm0[:, :num_train*int(np.size(ocm0[1])/5)]
-    ocm1_train = ocm1[:, :num_train*int(np.size(ocm1[1])/5)]
-    ocm2_train = ocm2[:, :num_train*int(np.size(ocm2[1])/5)]
-    ocm0_test = ocm0[:, num_train*int(np.size(ocm0[1])/5):-1]
-    ocm1_test = ocm1[:, num_train*int(np.size(ocm1[1])/5):-1]
-    ocm2_test = ocm2[:, num_train*int(np.size(ocm2[1])/5):-1]
+    ocm0_train = ocm0_pre[:, :num_train*int(np.size(ocm0_pre[1])/5)]
+    ocm1_train = ocm1_pre[:, :num_train*int(np.size(ocm1_pre[1])/5)]
+    ocm2_train = ocm2_pre[:, :num_train*int(np.size(ocm2_pre[1])/5)]
+    ocm0_test_pre = ocm0_pre[:, num_train*int(np.size(ocm0_pre[1])/5):-1]
+    ocm1_test_pre = ocm1_pre[:, num_train*int(np.size(ocm1_pre[1])/5):-1]
+    ocm2_test_pre = ocm2_pre[:, num_train*int(np.size(ocm2_pre[1])/5):-1]
 
-    # Combine three OCM
-    ocm_train = np.concatenate([ocm0_train, ocm1_train, ocm2_train], 0)
-    print('ocm_train shape', ocm_train.shape)
+    # Chose which data type you want
+    if ocm_channel is 'all':
+        # Combine three OCM
+        ocm_train = np.concatenate([ocm0_train, ocm1_train, ocm2_train], 0)
+        ocm_test_pre = np.concatenate([ocm0_test_pre, ocm1_test_pre, ocm2_test_pre], 0)
+        ocm_test_post = np.concatenate([ocm0_post, ocm1_post, ocm2_post], 0)
+    else:
+        # Use individual OCM
+        if ocm_channel is 0:
+            ocm_train = ocm0_train
+            ocm_test_pre = ocm0_test_pre
+            ocm_test_post = ocm0_post
+        elif ocm_channel is 1:
+            ocm_train = ocm1_train
+            ocm_test_pre = ocm1_test_pre
+            ocm_test_post = ocm1_post
+        elif ocm_channel is 2:
+            ocm_train = ocm2_train
+            ocm_test_pre = ocm2_test_pre
+            ocm_test_post = ocm2_post
 
+    print('ocm_train shape', ocm_train.shape) # (1050, 10110)
+    print('ocm_test_pre shape', ocm_test_pre.shape) # (1050, 6743)
+    print('ocm_test_post shape', ocm_test_post.shape) # (1050, 16484)
+    print(type(ocm_train))
 
+    x_train = ocm_train.T
+    #np.random.shuffle(x_train) # shuffle traces so that validation set is taken from each bh=1, 2, and 3
+    x_test = np.hstack([ocm_test_pre, ocm_test_post]).T
+    y_train = np.zeros((np.size(ocm_train[1])))
 
-csv = pd.read_csv('creditcard.csv')
-test_split = 0.3 # portion of data used for testing
-val_split = 0.2  # portion of training data used for validation
+    y_test_pre = np.zeros(np.size(ocm_test_pre[1])) # state 1
+    y_test_post = np.ones(np.size(ocm_test_post[1])) # state 2
+    y_test = np.concatenate([y_test_pre, y_test_post], axis=0)
 
-data = csv.astype('float32').copy()
-data.sort_values('Time', inplace=True)
-print('data shape', data.shape)
+    print('x_train shape', x_train.shape) # (10110, 1050)
+    print('x_test shape', x_test.shape) # (23227, 1050)
+    print('y_train shape', y_train.shape) # (10110,)
+    print('y_test shape', y_test.shape) # (23227,)
 
-first_test = int(data.shape[0] * (1 - test_split))
-first_val = int(first_test * (1 - val_split))
+    scaler = preprocessing.StandardScaler()
+    x_train, x_test = scaler.fit_transform(x_train), scaler.fit_transform(x_test)
 
-train = data.iloc[:first_val]
-val = data.iloc[first_val:first_test]
-test = data.iloc[first_test:]
+    # Build the model and print summary;
+    original_shape = x_train.shape[1:]
+    input_size = np.prod(original_shape)
+    latent_dim = 2  # number of latent variables to learn
+    hidden_size = 128  # size of the hidden layer in encoder and decoder
+    final_size = 64
 
-x_train_df, x_val_df, x_test_df = train.iloc[:, 1:-2], val.iloc[:, 1:-2], test.iloc[:, 1:-2]
-y_train_df, y_val_df, y_test_df = train.iloc[:, -1], val.iloc[:, -1], test.iloc[:, -1]
+    x = Input(shape=(input_size,))
+    t = BatchNormalization()(x)
+    t = Dense(hidden_size, activation='relu', name='encoder_hidden_h')(t)
+    t = BatchNormalization()(t)
+    t = Dense(final_size, activation='relu', name='encoder_hidden_f')(t)
+    t = BatchNormalization()(t)
 
-x_train, x_val, x_test = x_train_df.values, x_val_df.values, x_test_df.values
-y_train, y_val, y_test = y_train_df.values, y_val_df.values, y_test_df.values
+    z_mean = Dense(latent_dim, name='z_mean')(t)
+    z_log_var = Dense(latent_dim, name='z_log_var')(t)
 
-scaler = preprocessing.StandardScaler()
+    def sampling(args):
+        z_mean, z_log_var = args
+        epsilon = K.random_normal(shape=K.shape(z_mean), mean=0., stddev=1.)
+        return z_mean + K.exp(z_log_var / 2) * epsilon
 
-x_train, x_val, x_test = scaler.fit_transform(x_train), scaler.fit_transform(x_val), scaler.fit_transform(x_test)
-
-# Build the model and print summary;
-hidden_size = 16 #size of the hidden layer in encoder and decoder
-latent_dim = 2 #number of latent variables to learn
-
-input_dim = x_train.shape[1]
-
-x = Input(shape=(input_dim,))
-t = BatchNormalization()(x)
-t = Dense(hidden_size, activation='tanh' , name='encoder_hidden')(t)
-t = BatchNormalization()(t)
-
-z_mean = Dense(latent_dim, name='z_mean')(t)
-z_log_var = Dense(latent_dim, name='z_log_var')(t)
-
-def sampling(args):
-    z_mean, z_log_var = args
-    epsilon = K.random_normal(shape=K.shape(z_mean), mean=0., stddev=1.)
-    return z_mean + K.exp(z_log_var / 2) * epsilon
-
-z = Lambda(sampling, name='z_sampled')([z_mean, z_log_var])
-#t = BatchNormalization()(z)
-
-t = Dense(hidden_size, activation='tanh', name='decoder_hidden')(z)
-#t = BatchNormalization()(t)
-
-decoded_mean = Dense(input_dim, activation=None, name='decoded_mean')(t)
-
-vae = Model(x, decoded_mean)
-
-def rec_loss(y_true, y_pred):
-    return K.sum(K.square(y_true - y_pred), axis=-1)
-
-def kl_loss(y_true, y_pred):
-    return - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
-
-def vae_loss(x, decoded_mean):
-    rec_loss = K.sum(K.square(x - decoded_mean), axis=-1)
-    kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
-    return K.mean((rec_loss + kl_loss) / 2)
-
-vae.compile(optimizer=Adam(lr=1e-2), loss=vae_loss, metrics=[rec_loss, kl_loss])
-vae.summary()
-plot_model(vae, to_file='vae_mlp.png', show_shapes=True)
-
-# train model
-n_epochs = 30
-batch_size = 128
-
-early_stopping = EarlyStopping(monitor='loss', patience=10, min_delta=1e-5) #stop training if loss does not decrease with at least 0.00001
-reduce_lr = ReduceLROnPlateau(monitor='loss', patience=5, min_delta=1e-5, factor=0.2) #reduce learning rate (divide it by 5 = multiply it by 0.2) if loss does not decrease with at least 0.00001
-
-callbacks = [early_stopping, reduce_lr]
-
-#collect training data in history object
-history = vae.fit(x_train, x_train,
-                  validation_data=(x_val, x_val),
-                  batch_size=batch_size, epochs=n_epochs,
-                  callbacks=callbacks)
+    z = Lambda(sampling, name='z_sampled')([z_mean, z_log_var])
 
 
-fig = plt.figure(figsize=(14, 6))
-ax = fig.gca()
-ax.plot(history.history['rec_loss']);
-ax.plot(history.history['val_rec_loss']);
+    decoder_f = Dense(final_size, activation='relu', name='decoder_hidden_f')
+    decoder_h = Dense(hidden_size, activation='relu', name='decoder_hidden_h')
+    decoder_mean = Dense(input_size, activation='sigmoid', name='decoded_mean')
 
-x_data = x_train
-y_data = y_train
+    f_decoded = decoder_f(z)
+    h_decoded = decoder_h(f_decoded)
+    x_decoded_mean = decoder_mean(h_decoded)
 
-encoder = Model(x, z_mean)
-encoder.summary()
-plot_model(encoder, to_file='vae_mlp_encoder.png', show_shapes=True)
+    # instantiate VAE model
+    vae = Model(x, x_decoded_mean)
 
-with_labels = np.concatenate([x_data, np.reshape(y_data, (-1, 1))], axis=1) #concatenate x and y to be able to filter by class
+    # Compute VAE loss
+    def rec_loss(y_true, y_pred):
+        return K.sum(K.square(y_true - y_pred), axis=-1)
 
-normal = with_labels[np.where(with_labels[:, -1] == 0)] #filter normal instances
-anomalous = with_labels[np.where(with_labels[:, -1] == 1)] #filter anomalous instances
+    def kl_loss(y_true, y_pred):
+        return - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
 
-normal_encoded = encoder.predict(normal[:, :-1], batch_size=128)
-anomalous_encoded = encoder.predict(anomalous[:, :-1], batch_size=128)
+    def vae_loss(x, decoded_mean):
+        rec_loss = K.sum(K.square(x - decoded_mean), axis=-1)
+        kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+        return K.mean((rec_loss + kl_loss) / 2)
 
-fig = plt.figure(figsize=(6, 6))
-ax = fig.gca()
+    vae.compile(optimizer=Adam(lr=1e-2), loss=vae_loss, metrics=[rec_loss, kl_loss])
+    vae.summary()
+    plot_model(vae, to_file='vae_ocm.png', show_shapes=True)
 
-#semicolon at the end hides unnecessary output
-ax.scatter(normal_encoded[:, 0], normal_encoded[:, 1]);
-ax.scatter(anomalous_encoded[:, 0], anomalous_encoded[:, 1]);
-fig.savefig('result.png')
+    # train model
+    n_epochs = 30
+    batch_size = 128
 
-# Apply simple Linear Discriminant Analysis to learned features (the latent representations):
-with_labels_encoded = encoder.predict(with_labels[:, :-1], batch_size=128)
+    early_stopping = EarlyStopping(monitor='loss', patience=10, min_delta=1e-5) #stop training if loss does not decrease with at least 0.00001
+    reduce_lr = ReduceLROnPlateau(monitor='loss', patience=5, min_delta=1e-5, factor=0.2) #reduce learning rate (divide it by 5 = multiply it by 0.2) if loss does not decrease with at least 0.00001
 
-X = with_labels_encoded
-y = with_labels[:, -1]
+    callbacks = [early_stopping, reduce_lr]
 
-clf = LinearDiscriminantAnalysis()
-clf.fit(X, y)
+    #collect training data in history object
+    history = vae.fit(x_train, x_train,
+                      #validation_data=(x_val, x_val),
+                      batch_size=batch_size, epochs=n_epochs,
+                      callbacks=callbacks)
 
-pred = clf.predict(X)
+    encoder = Model(x, z_mean)
+    # display a 2D plot of the classes in the latent space
+    X_test_encoded = encoder.predict(x_test, batch_size=batch_size)
 
-print("AUC(ROC): " + str(metrics.roc_auc_score(y, pred)))
-print("Precision: " + str(metrics.precision_score(y, pred)))
-print("Recall: " + str(metrics.recall_score(y, pred)))
-print("F1 score: " + str(metrics.f1_score(y, pred)))
+    fig = plt.figure(figsize=(6, 6))
+    ax = fig.gca()
+    plt.scatter(X_test_encoded[:, 0], X_test_encoded[:, 1], c=y_test, alpha=0.3, s=10)
+    #plt.scatter(X_test_encoded[:, 0], X_test_encoded[:, 1], facecolors=y_test, edgecolors=y_test, s=10)
+    plt.colorbar()
+    fig.savefig('result_2d_fidx'+str(fidx)+'_ch'+str(ocm_channel)+'.png')
 
-tn, fp, fn, tp = metrics.confusion_matrix(y, pred).ravel()
 
-print("False positives: " + str(fp))
-print("True positives: " + str(tp))
-print("False negatives: " + str(fn))
-print("True negateives: " + str(tn))
+    x_data = x_train
+    y_data = y_train
+    encoder = Model(x, z_mean)
+    with_labels = np.concatenate([x_data, np.reshape(y_data, (-1, 1))], axis=1) #concatenate x and y to be able to filter by class
+
+    # Apply simple Linear Discriminant Analysis to learned features (the latent representations):
+    with_labels_encoded = encoder.predict(with_labels[:, :-1], batch_size=128)
+
+    X = with_labels_encoded
+    y = with_labels[:, -1]
+
+    clf = LinearDiscriminantAnalysis()
+    clf.fit(X, y)
+
+    pred = clf.predict(X)
+
+#    print("AUC(ROC): " + str(metrics.roc_auc_score(y, pred)))
+    print("Precision: " + str(metrics.precision_score(y, pred)))
+    print("Recall: " + str(metrics.recall_score(y, pred)))
+    print("F1 score: " + str(metrics.f1_score(y, pred)))
+
+    tn, fp, fn, tp = metrics.confusion_matrix(y, pred).ravel()
+
+    print("False positives: " + str(fp))
+    print("True positives: " + str(tp))
+    print("False negatives: " + str(fn))
+    print("True negateives: " + str(tn))
+
